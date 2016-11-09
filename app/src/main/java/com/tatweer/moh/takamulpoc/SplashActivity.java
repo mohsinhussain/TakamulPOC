f577a1263f5980f4680d459dde936e80d737ef4a
Mohsin Hussain <m.hussain@tatweer-co.ae>
2016-11-09 14:14:38 +0400

    Splash and main activity layout set

--- app/src/main/java/com/tatweer/moh/takamulpoc/SplashActivity.java
+++ app/src/main/java/com/tatweer/moh/takamulpoc/SplashActivity.java
@@ -1,13 +1,42 @@
 package com.tatweer.moh.takamulpoc;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 
 public class SplashActivity extends AppCompatActivity {
+
+    // Splash screen timer
+    private static int SPLASH_TIME_OUT = 3000;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+
+        new Handler().postDelayed(new Runnable() {
+
+            /*
+             * Showing splash screen with a timer. This will be useful when you
+             * want to show case your app logo / company
+             */
+
+            @Override
+            public void run() {
+                // This method will be executed once the timer is over
+                // Start your app main activity
+                openMainActivity();
+                finish();
+            }
+        }, SPLASH_TIME_OUT);
+    }
+
+    private void openMainActivity()
+    {
+        Intent out = new Intent(this, MainActivity.class);
+        startActivity(out);
+
+        finish();
     }
 }
